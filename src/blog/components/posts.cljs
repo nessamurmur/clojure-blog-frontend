4b3ab443892187b12f66512f6dffa27a3fbd6779
Nate West <natescott.west@gmail.com>
2014-10-09 09:22:09 -0500

    Fix posts-view styling

--- src/blog/components/posts.cljs
+++ src/blog/components/posts.cljs
@@ -6,20 +6,19 @@
   (reify
     om/IRender
     (render [this]
-      (dom/div #js {:className "post"}
-        (dom/section nil
-          (dom/div #js {:className "post-preview col-xs-10  no-gutter"}
-            (dom/h2 nil
-              (dom/a #js {:href "post.html"} (:title post)))
-            (dom/p nil (:body post))
-            (dom/p #js {:className "meta"}
-              (dom/a #js {:href "category.html"} (:category post))
-              (dom/i #js {:className "link-spacer"})
-              (dom/i #js {:className "entypo-bookmark"} (str " " (:created-at post))))))))))
+      (dom/section nil
+        (dom/div #js {:className "post-preview col-xs-10  no-gutter"}
+          (dom/h2 nil
+            (dom/a #js {:href "post.html"} (:title post)))
+          (dom/p nil (:body post))
+          (dom/p #js {:className "meta"}
+            (dom/a #js {:href "category.html"} (:category post))
+            (dom/i #js {:className "link-spacer"})
+            (dom/i #js {:className "entypo-bookmark"} (str " " (:created-at post)))))))))
 
 (defn posts-view [app owner]
   (reify
     om/IRender
     (render [this]
-      (apply dom/ul nil
+      (apply dom/div #js {:className "post"}
         (om/build-all post-view (:posts app))))))
