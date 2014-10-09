f14731233f7b4c6661239dab184ffe7025d3f45e
Nate West <natescott.west@gmail.com>
2014-10-09 08:27:00 -0500

    Render posts

--- src/blog/core.cljs
+++ src/blog/core.cljs
@@ -10,33 +10,36 @@
   (atom
     {:posts
      [{:title "Hello world" :body "Hello there!"
-       :tags ["hello-word" "clojure"] :url "http://google.com"
+       :category "clojure" :url "http://google.com"
        :created-at "10/10/2014" :updated-at "10/10/2014"}
       {:title "Hello world" :body "Hello there!"
-       :tags ["hello-word" "clojure"] :url "http://google.com"
+       :category "clojure" :url "http://google.com"
        :created-at "10/10/2014" :updated-at "10/10/2014"}
       {:title "Hello world" :body "Hello there!"
-       :tags ["hello-word" "clojure"] :url "http://google.com"
+       :category "clojure" :url "http://google.com"
        :created-at "10/10/2014" :updated-at "10/10/2014"}]}))
 
 (defn post-view [post owner]
   (reify
-    om/IRenderState
-    (render-state [this {:keys [delete]}]
-      (dom/article nil
-        (dom/span #js {:className "pull-right"} (:created-at post))
-        (dom/h2 #js {:className "post-title"}
-                (dom/a #js {:href (:url post)} (:title post)))
-        (dom/p nil (:body post))))))
+    om/IRender
+    (render [this]
+      (dom/div #js {:className "post"}
+        (dom/section nil
+          (dom/div #js {:className "post-preview col-xs-10  no-gutter"}
+            (dom/h2 nil
+              (dom/a #js {:href "post.html"} (:title post)))
+            (dom/p nil (:body post))
+            (dom/p #js {:className "meta"}
+              (dom/a #js {:href "category.html"} (:category post))
+              (dom/i #js {:className "link-spacer"})
+              (dom/i #js {:className "entypo-bookmark"} (str " " (:created-at post))))))))))
 
 (defn posts-view [app owner]
   (reify
     om/IRender
     (render [this]
-      (dom/div #js {:className "container"}
-        (dom/h1 #js {:className "blog-title"} "Blog")
-        (apply dom/ul nil
-           (om/build-all post-view (:posts app)))))))
+      (apply dom/ul nil
+        (om/build-all post-view (:posts app))))))
 
 (om/root posts-view app-state
          {:target (. js/document (getElementById "posts"))})
