0095e88898fb0b6e5d4da03b281a04ad0c959f4a
Nate West <natescott.west@gmail.com>
2014-10-09 10:23:52 -0500

    Properly wrap post divs

--- src/blog/components/posts.cljs
+++ src/blog/components/posts.cljs
@@ -13,18 +13,19 @@
   (reify
     om/IRender
     (render [this]
-      (dom/section nil
-        (dom/div #js {:className "post-preview col-xs-10  no-gutter"}
-          (dom/h2 nil
-            (dom/a #js {:href "post.html"} (:title post)))
-          (dom/p nil (:body post))
-          (dom/p #js {:className "meta"}
-            (dom/a #js {:href "category.html"} (get-category post))
-            (dom/i #js {:className "entypo-bookmark"} (get-timestamp post))))))))
+      (dom/div #js {:className "post"}
+        (dom/section nil
+          (dom/div #js {:className "post-preview col-xs-10  no-gutter"}
+            (dom/h2 nil
+              (dom/a #js {:href "post.html"} (:title post)))
+            (dom/p nil (:body post))
+            (dom/p #js {:className "meta"}
+              (dom/a #js {:href "category.html"} (get-category post))
+              (dom/i #js {:className "entypo-bookmark"} (get-timestamp post)))))))))
 
 (defn posts-view [app owner]
   (reify
     om/IRender
     (render [this]
-      (apply dom/div #js {:className "post"}
+      (apply dom/div nil
         (om/build-all post-view (:posts app))))))
